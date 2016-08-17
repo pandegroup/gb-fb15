43a1f51c4b412c209ee1e46b40af9833eb3e87fd
kmckiern <kmckiern@stanford.edu>
2016-08-17 16:46:17 -0700

    most of rendering settings

--- vis/bilayer_img.py
+++ vis/bilayer_img.py
@@ -1,16 +1,11 @@
 import os
 from chimera import runCommand as rc 
 from chimera import replyobj 
-import numpy as np
-import glob
 
 gro_file = '../gromacs/ic.gro'
-
 rc('open ' + gro_file)
-
 rc("preset apply publication 1")
 rc("light mode ambient")
 rc("rep bs")
 rc("setattr m stickScale .2")
 rc("setattr m ballScale .44")
-
